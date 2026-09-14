b98ad9e6114380e08b1710ed80720200e563d0be
meheraj786 <meherajhosen786@gmail.com>
2026-09-14 23:31:54 +0600

    backend db update

--- backend/src/auth/mail.service.ts
+++ backend/src/auth/mail.service.ts
@@ -22,7 +22,7 @@ export class MailService {
         ? nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: Number(process.env.SMTP_PORT ?? 587),
-            secure: Number(process.env.SMTP_PORT) === 465,
+            secure: true,
             auth: {
               user: process.env.SMTP_USER,
               pass: process.env.SMTP_PASSWORD,
